936d71511900b0f5bd864b7837a40ccae3899df1
Zhang Yi Jiang <zhangyijiang@protonmail.com>
2026-07-29 11:48:08 -0700

    Use @default tag in JSDoc for expiration minutes fields

--- packages/core/src/public/b2b/ui.ts
+++ packages/core/src/public/b2b/ui.ts
@@ -49,15 +49,18 @@ export type B2BEmailMagicLinksOptions = {
   domainHint?: string;
   locale?: string;
   /**
-   * The number of minutes a login Magic Link is valid for. Defaults to 60. Min: 5, Max: 10080.
+   * The number of minutes a login Magic Link is valid for. Min: 5, Max: 10080.
+   * @default 60
    */
   loginExpirationMinutes?: number;
   /**
-   * The number of minutes a signup Magic Link is valid for. Defaults to 60. Min: 5, Max: 10080.
+   * The number of minutes a signup Magic Link is valid for. Min: 5, Max: 10080.
+   * @default 60
    */
   signupExpirationMinutes?: number;
   /**
-   * The number of minutes a Discovery Magic Link is valid for. Defaults to 60. Min: 5, Max: 10080.
+   * The number of minutes a Discovery Magic Link is valid for. Min: 5, Max: 10080.
+   * @default 60
    */
   discoveryExpirationMinutes?: number;
 };
